7462bf9babf522ab37bf6480f675d56c8dc0c123
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2023-02-27 12:54:04 +0100

    adapt example for cloudflare pages

--- functions/[[remix]].ts
+++ functions/[[remix]].ts
@@ -1,12 +1,24 @@
 import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "../build";
+const { createStorefrontClient } = require("@shopify/hydrogen");
 
 let remixHandler: ReturnType<typeof createRequestHandler>;
 
-export const onRequest: PagesFunction<Env> = (context) => {
+export const onRequest: PagesFunction<Env> = ({ request, env }) => {
+  console.log(">>>>>>>", env);
+  const { storefront } = createStorefrontClient({
+    // buyerIp: getBuyerIp(request),
+    i18n: { language: "EN", country: "US" },
+    publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+    privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+    storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+    storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || "2023-01",
+    storefrontId: env.PUBLIC_STOREFRONT_ID,
+    // requestGroupId: req.headers.get('request-id'),
+  });
   if (!remixHandler) {
-    remixHandler = createRequestHandler(build, context.env.ENVIRONMENT);
+    remixHandler = createRequestHandler(build, env.ENVIRONMENT);
   }
   // This is where you can pass a custom load context to your app
-  return remixHandler(context.request);
+  return remixHandler(request, { storefront });
 };
